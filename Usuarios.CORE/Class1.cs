b2d10ff98cc64aff860549343f91bf1f946c1e5b
Mateo Betancur <mateo-beta@hotmail.com>
2020-10-28 22:30:06 -0500

    Carga inicial del proyecto

---
+++ Usuarios.CORE/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Usuarios.CORE
+{
+    public class Class1
+    {
+    }
+}
---
+++ Usuarios.INFRASTRUCTURE/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Usuarios.INFRASTRUCTURE
+{
+    public class Class1
+    {
+    }
+}
